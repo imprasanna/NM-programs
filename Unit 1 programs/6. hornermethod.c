18fb883b7a06e385b45a4c329d1f618b8e709920
Prasanna <prasannaacharya2073@gmail.com>
2023-01-30 10:22:58 +0545

    first commit

---
+++ Unit 1 programs/6. hornermethod.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+ float a[6],    b[6],x;
+ 
+ int i,n;
+  printf("Enter the highest degree of the equation : ");
+  scanf("%d", &n);
+
+   for (i = n; i >=0; i--)
+  {
+    printf("\n Coefficient a[%d] = ", i);
+    scanf("%f", &a[i]);
+
+  }
+
+  printf("Enter the value of x at which we need to find the functional value of x : ");
+  scanf("%f",&x);
+
+   b[n]=a[n];
+
+  while(n>0){
+    b[n-1]=a[n-1]+ b[n]* x;
+
+    n--;
+
+  }
+ printf("\nThe value of the polynomial at x=%.2f is:  %.3f \n\n",x,b[0]);
+
+}
